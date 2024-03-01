6bd7f5b65acac390dd5e36f12452b1e1a14e3145
cryptoswiftapp <cryptoswiftapp@gmail.com>
2024-03-01 12:45:10 +0100

    Update: bg image

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ const theme = extendTheme({
   styles: {
     global: {
       "html, body": {
-        backgroundImage: "url('./background.png')",
+        backgroundImage: "url('./background1.png')",
         height: "100%",
         width: "100%",
         backgroundSize: "cover",
